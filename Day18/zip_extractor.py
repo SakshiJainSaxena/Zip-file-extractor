b940b6fb3668d5866727835efb1fbc050d5bc9a5
officialvishalsaxena0802@gmail.com <officialvishalsaxena0802@gmail.com>
2023-07-14 18:55:00 +0530

    Zip File Extractor Desktop App

--- Day18/zip_extractor.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-
-def extract_archive(archivepath, destination_folder):
-    with zipfile.ZipFile(archivepath, 'r') as archive:
-        archive.extractall(destination_folder)
-
-
-if __name__ == "__main__":
-    print("Code is working.")
